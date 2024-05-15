d5472609cbd3aa4a7c9802cc5849f18dbfaae0fb
l2d <liumingyuan_nuc@163.com>
2024-05-15 08:57:50 +0800

    add mesh component

--- engine/src/runtime/core/framework/component/mesh_component/mesh_component.h
+++ engine/src/runtime/core/framework/component/mesh_component/mesh_component.h
@@ -5,27 +5,29 @@
 
 namespace peanut
 {
-	class META_CLASS() MeshComponent : public Component
+	class META_CLASS() StaticMeshComponent : public Component
 	{
-		GENERATE_BODY(MeshComponent)
+		GENERATE_BODY(StaticMeshComponent)
 	public:
 
-		MeshComponent() = default;
-		virtual ~MeshComponent() = default;
+		StaticMeshComponent() = default;
+		virtual ~StaticMeshComponent() = default;
 
 		META_FUNCTION()
 		virtual void Tick(float delta_time) override {}
 
-		// void LoadStaticMesh(const std::string& path);
-
-		// void SetMaterial(const Material& material) { material_ = material; }
-
 		// void SetStaticMesh(std::shared_ptr<StaticMesh> static_mesh) { static_mesh_ = static_mesh; }
+
+		// std::shared_ptr<StaticMesh> GetStaticMesh() const { return static_mesh_; }
+
+		// std::shared_ptr<Material> GetMaterial() const { return material_; }
 
 	private:
 		// std::shared_ptr<StaticMesh> static_mesh_;
 
-		// Material material_;
+		// std::shared_ptr<Material> material_;
+
+		// BoundingBox bounding_box_;
 
 	};
 } // namespace peanut
